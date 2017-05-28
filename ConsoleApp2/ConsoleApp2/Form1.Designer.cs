772c43e6ae838b1ef2367a5bb41e15da30ae6d61
Aidan <aidan79225@gmail.com>
2017-05-28 20:43:41 +0800

    add new

---
+++ ConsoleApp2/ConsoleApp2/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace ConsoleApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(297, 12);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(393, 521);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(297, 566);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 67);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "開啟輸入檔案";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 20F);
+            this.textBox1.Location = new System.Drawing.Point(163, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(119, 39);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "12";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 27);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "observation";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label2.Location = new System.Drawing.Point(88, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 27);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "skip";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label3.Location = new System.Drawing.Point(52, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 27);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "holding";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 20F);
+            this.label4.Location = new System.Drawing.Point(12, 246);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 27);
+            this.label4.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("新細明體", 20F);
+            this.textBox2.Location = new System.Drawing.Point(163, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(119, 39);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "0";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("新細明體", 20F);
+            this.textBox3.Location = new System.Drawing.Point(163, 169);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(119, 39);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(140, 566);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 67);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "計算";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 662);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp2
+{
+    public partial class Form1 : Form
+    {
+        List<Company> companys = new List<Company>();
+        int observation = 12;
+        int holding = 1;
+        int skip = 0;
+        int priceSize = 4696;
+        string filePath = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ColumnHeader header1 = new ColumnHeader();
+            header1.Text = "";
+            header1.TextAlign = HorizontalAlignment.Left;
+            header1.Width = 700;
+            listView1.Columns.Add(header1);
+            listView1.View = View.Details;
+
+           
+
+        }
+
+        
+        
+
+        public class Company : IComparable<Company>
+        {
+            double compare = 0;
+            double rateOfReturn = -1;
+            double[] companyPrice;
+            int initIndex = 0;
+            public Company(double[] companyPrice, int observation, int holding, int skip)
+            {
+                this.companyPrice = companyPrice;
+                computeCompareValue(this.companyPrice, observation);
+                computeRateOfReturn(this.companyPrice, observation, holding, skip);
+            }
+            public void reload(int initIndex,int observation, int holding, int skip)
+            {
+                this.initIndex = initIndex;
+                computeCompareValue(this.companyPrice, observation);
+                computeRateOfReturn(this.companyPrice, observation, holding, skip);
+            }
+
+
+            int IComparable<Company>.CompareTo(Company other)
+            {
+                return compare - other.compare > 0 ? 1 : compare - other.compare == 0 ? 0 : -1;
+            }
+
+            private void computeCompareValue(double[] companyPrice, int observation)
+            {
+                double max = 0;
+                int size = Math.Min(observation+ initIndex, companyPrice.Length);
+                for (int i = initIndex; i < size; i++)
+                {
+                    if (companyPrice[i] > max)
+                    {
+                        max = companyPrice[i];
+                    }
+                }
+                compare = companyPrice[initIndex+observation - 1] / max;
+            }
+            private void computeRateOfReturn(double[] companyPrice, int observation, int holding, int skip)
+            {
+                int first = Math.Min((initIndex + observation + skip -1), companyPrice.Length);
+                int last = Math.Min((initIndex + observation + skip + holding-1), companyPrice.Length);
+                rateOfReturn = (companyPrice[last] - companyPrice[first]) / companyPrice[first];
+
+            }
+
+            public string getString()
+            {
+                return "compare :" + compare + ", rate : " + rateOfReturn;
+            }
+            public bool hasZero(int observation)
+            {
+                int size = Math.Min(companyPrice.Length, observation + initIndex);
+                for (int i = initIndex; i < size; i++)
+                {
+                    if (companyPrice[i] == 0)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog f = new OpenFileDialog();
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                filePath = f.FileName;
+                loadDataAndCompute();
+            }
+
+        }
+        private void loadDataAndCompute()
+        {
+            observation = Convert.ToInt32(textBox1.Text);
+            skip = Convert.ToInt32(textBox2.Text);
+            holding = Convert.ToInt32(textBox3.Text);
+            listView1.Clear();
+            ColumnHeader header1 = new ColumnHeader();
+            header1.Text = "內容";
+            header1.TextAlign = HorizontalAlignment.Left;
+            header1.Width = 700;
+            listView1.Columns.Add(header1);
+            listView1.View = View.Details;
+            StringBuilder sb = new StringBuilder();
+            string line;
+            companys.Clear();
+            StreamReader sr = new StreamReader(filePath);
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] h = line.Split(',');
+                priceSize = h.Length;
+                double[] tempDouble = new double[priceSize];
+                int priceIndex = 0;
+                foreach (string t in h)
+                {
+                    tempDouble[priceIndex] = Convert.ToDouble(t);
+                    priceIndex++;
+                }
+                companys.Add(new Company(tempDouble, observation, holding, skip));
+            }
+            
+            int initIndex = 0;
+            while (initIndex + observation + skip + holding < priceSize)
+            {
+                ListViewItem i = new ListViewItem();
+                i.Text = "-------"+ initIndex + "---------";
+                listView1.Items.Add(i);
+                foreach (Company c in companys)
+                {
+                    c.reload(initIndex, observation, holding, skip);
+                }
+                List<Company> tempCompanys = new List<Company>();
+                foreach (Company c in companys)
+                {
+                    if (!c.hasZero(observation))
+                    {
+                        tempCompanys.Add(c);
+                    }
+                }
+                tempCompanys.Sort();
+                foreach (Company c in tempCompanys)
+                {
+                    ListViewItem ii = new ListViewItem();
+                    ii.Text = c.getString();
+                    listView1.Items.Add(ii);
+                }
+                initIndex++;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            loadDataAndCompute();
+        }
+    }
+}
